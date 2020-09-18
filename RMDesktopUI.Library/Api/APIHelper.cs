7a5830f44736500a8a9020fd157ab1e599710189
Adrian Radulescu <adrian@lundin.fo>
2020-09-18 10:50:25 +0300

    Added UI Library
    - connects out UI to the API

---
+++ RMDesktopUI.Library/Api/APIHelper.cs
@@ -0,0 +1,87 @@
+﻿using RMDesktopUI.Library.Models;
+using RMDesktopUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMDesktopUI.Library.Api
+{
+    public class APIHelper : IAPIHelper
+    {
+        private HttpClient apiClient { get; set; }
+        private ILoggedInUserModel _loggedInUser;
+
+        public APIHelper(ILoggedInUserModel loggedInUser)
+        {
+            InitializeClient();
+            _loggedInUser = loggedInUser;
+        }
+
+        private void InitializeClient()
+        {
+            string api = ConfigurationManager.AppSettings["api"];
+
+            //request HEADERS
+            apiClient = new HttpClient();
+            apiClient.BaseAddress = new Uri(api);
+            apiClient.DefaultRequestHeaders.Accept.Clear();
+            apiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<AuthenticatedUser> Authenticate(string username, string password)
+        {
+            //create contents
+            var data = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string,string>("grant_type","password"),
+                new KeyValuePair<string,string>("username",username),
+                new KeyValuePair<string,string>("password",password)
+            });
+
+            //POST to the TOKEN
+            using (HttpResponseMessage response = await apiClient.PostAsync("/Token", data))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsAsync<AuthenticatedUser>();
+                    return result;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+
+        public async Task GetLoggedInUserInfo(string token)
+        {
+            apiClient.DefaultRequestHeaders.Clear();
+            apiClient.DefaultRequestHeaders.Accept.Clear();
+            apiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            apiClient.DefaultRequestHeaders.Add("Authorization", $"Bearer { token }");
+
+            using (HttpResponseMessage response = await apiClient.GetAsync("/api/User"))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsAsync<LoggedInUserModel>();
+                    _loggedInUser.Token = token;
+                    _loggedInUser.Id = result.Id;
+                    _loggedInUser.FirstName = result.FirstName;
+                    _loggedInUser.LastName = result.LastName;
+                    _loggedInUser.EmailAddress = result.EmailAddress;
+                    _loggedInUser.CreatedDate = result.CreatedDate;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+    }
+}
