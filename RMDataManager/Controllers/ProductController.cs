768682399fe359a2deacd19d4be49369eab49a79
Adrian Radulescu <adrian@lundin.fo>
2020-09-24 21:09:58 +0300

    Add User Roles
    Add authorization by role to the endpoints

--- RMDataManager/Controllers/ProductController.cs
+++ RMDataManager/Controllers/ProductController.cs
@@ -10,7 +10,7 @@ using System.Web.Http;
 
 namespace RMDataManager.Controllers
 {
-    [Authorize]
+    [Authorize(Roles = "Cashier")]
     public class ProductController : ApiController
     {
         public List<ProductModel> Get()
