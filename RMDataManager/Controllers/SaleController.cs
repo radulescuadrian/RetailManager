65fd693397969fb546ef99da7b5e664dd70317c0
Adrian Radulescu <adrian@lundin.fo>
2020-09-24 19:36:25 +0300

    Add admin endpoint data
    - adds the data for administrative endpoints, does not secure the endpoints beyond authorizations

--- RMDataManager/Controllers/SaleController.cs
+++ RMDataManager/Controllers/SaleController.cs
@@ -20,5 +20,12 @@ namespace RMDataManager.Controllers
 
             data.SaveSale(sale, userId);
         }
+
+        [Route("GetSalesReport")]
+        public List<SaleReportModel> GetSalesReport()
+        {
+            SaleData data = new SaleData();
+            return data.GetSaleReport();
+        }
     }
 }
